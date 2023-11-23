d0a624f1341b992ea22feec11952417598d8190c
Jao <91709412+21520772@users.noreply.github.com>
2023-11-23 11:07:54 -0500

    Add files via upload

---
+++ New folder/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      spacing: {
+        900: "900px",
+        230: "230px",
+      }
+    },
+  },
+  plugins: [],
+}
+
